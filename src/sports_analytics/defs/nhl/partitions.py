3986421d9869204d27f207b7fe431fb418760447
Klemens Graf <klemensgraf15@gmail.com>
2026-02-05 11:03:54 +0100

    feat: daily update schedule (#11)
    * job definition and daily update schedule

--- src/sports_analytics/defs/nhl/partitions.py
+++ src/sports_analytics/defs/nhl/partitions.py
@@ -2,4 +2,6 @@ import dagster as dg
 
 from sports_analytics.defs.nhl.constants import GAMES_START_DATE
 
-games_daily_partition = dg.DailyPartitionsDefinition(start_date=GAMES_START_DATE)
+games_daily_partition = dg.DailyPartitionsDefinition(
+    start_date=GAMES_START_DATE, timezone="Europe/Zurich"
+)
